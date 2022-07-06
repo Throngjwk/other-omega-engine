9e25ed75aca0e4235f58cb4075777dc4524f07e9
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-06 11:15:11 -0300

    rename

--- js/components/incremental_mass-layer.js
+++ js/components/incremental_mass-layer.js
@@ -22,8 +22,8 @@ Vue.component("incremental-mass-layer", {
     template: `<div class="incremental-mass-layer">
     <div v-if="unlockedIncrementalMass">
     <div class="resource">
-        <p>You have {{formatNumber(incrementalMass.IMpoints, 2, 2, 1e9)}} <span class="aleph">ℵ</span></p>
-        <p>You get {{formatNumber(incrementalMass.getIncrementalMassGain(), 2, 2, 1e9)}} <span class="aleph">ℵ</span> every second</p>
+        <p>You have {{formatNumber(incrementalMass.IMpoints, 2, 2, 1e9)}} <span class="omega">IM</span></p>
+        <p>You get {{formatNumber(incrementalMass.getIncrementalMassGain(), 2, 2, 1e9)}} <span class="omega">IM</span> every second</p>
 </div>
 <div class="upgrades">
         <incremental-mass-upgrade :upgrade="incrementalMass.upgrades.IMGain"></incremental-mass-upgrade>
